2a451d109088ceed6a3ace416263a6b8842d790f
Arthur Yan <arthur.yan@datadoghq.com>
2014-11-13 16:55:24 +0000

    Test new pickle storage path

--- checks/check_status.py
+++ checks/check_status.py
@@ -251,7 +251,21 @@ class AgentStatus(object):
     @classmethod
     def _get_pickle_path(cls):
         log.info("the pickle path")
-        return os.path.join(tempfile.gettempdir(), cls.__name__ + '.pickle')
+        import ctypes
+        from ctypes import wintypes, windll
+
+        CSIDL_COMMON_APPDATA = 35
+
+        _SHGetFolderPath = windll.shell32.SHGetFolderPathW
+        _SHGetFolderPath.argtypes = [wintypes.HWND,
+                                    ctypes.c_int,
+                                    wintypes.HANDLE,
+                                    wintypes.DWORD, wintypes.LPCWSTR]
+
+        path_buf = wintypes.create_unicode_buffer(wintypes.MAX_PATH)
+        result = _SHGetFolderPath(0, CSIDL_COMMON_APPDATA, 0, 0, path_buf)
+        return path_buf.value
+        # return os.path.join(tempfile.gettempdir(), cls.__name__ + '.pickle')
 
 
 class InstanceStatus(object):
